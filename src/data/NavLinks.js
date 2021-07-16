b093128a7f78d9ac8142033f448f20ca23a0541f
Piero <piero.onorati@virgilio.it>
2021-07-16 15:56:33 +0200

    scaffolding

---
+++ src/data/NavLinks.js
@@ -0,0 +1,41 @@
+export default[
+    {   
+        link: 'Home',
+        url:'#',
+        active:true,
+        dropdown:['Home - Alternate']
+    },
+    {   
+        link: 'Shop',
+        url:'#',
+        active:false,
+        dropdown:[
+            'Shop no sidebar',
+            'Shop single sidebar',
+            'Shop dual sidebar',
+            'Simple product',
+            'Variable product'
+        ]
+    },
+    {   
+        link: 'About',
+        url:'#',
+        active:false
+    },
+    {   
+        link: 'Blog',
+        url:'#',
+        active:false
+    },
+    {   
+        link: 'Contact',
+        url:'#',
+        active:false
+    },
+    {   
+        link: 'Shop by brand',
+        url:'#',
+        active:false
+    }
+
+]
